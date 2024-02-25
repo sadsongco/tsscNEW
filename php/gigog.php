27a7382d5c5cfdb9035df73dc69c5c865782d54a
Nigel Powell <nigel@thesadsongco.com>
2024-02-25 12:16:29 +0000

    Gigography filters unstyled

--- php/gigog.php
+++ php/gigog.php
@@ -3,28 +3,73 @@
 require_once("includes/std_g_includes.php");
 include_once("includes/print_r2.php");
 
+$render_params = [];
+$sql_filter = "";
+$params=[];
+
+function get_filter_name($db, $col, $tbl, $filter, $id) {
+    try {
+        $query = "SELECT $col FROM $tbl WHERE $filter = ?";
+        $stmt = $db->prepare($query);
+        $stmt->execute([$id]);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result[0][$col];
+    }
+    catch(PDOException $e) {
+        error_log("Couldn't retrieve band name: ".$e->getMessage());
+    }
+}
+
+if (isset($_GET['filter_country']) && $_GET['filter_country'] != "") {
+    $sql_filter .= "\tAND shows.country_id = :filter_country";
+    $params["filter_country"] = $_GET['filter_country'];
+    $render_params["filter_country"] = $_GET['filter_country'];
+    $render_params["filter_country_name"] = get_filter_name($db, "disp_name", "countries", "country_id", $_GET['filter_country']);
+}
+
+if (isset($_GET['filter_band']) && $_GET['filter_band'] != "") {
+    $sql_filter .= "\n\tAND shows.band_id = :filter_band";
+    $params["filter_band"] = $_GET['filter_band'];
+    $render_params["filter_band"] = $_GET['filter_band'];
+    $render_params["filter_band_name"] = get_filter_name($db, "band_name", "bands", "band_id", $_GET['filter_band']);
+}
+
+
+$search = "";
+if (isset($_GET) && isset($_GET['search'])) {
+    $render_params['search'] = $_GET['search']; // sanitise this
+    $search_term = "%".$_GET['search']."%";
+    $search = "\tAND 
+    (event LIKE :search
+    OR venue LIKE :search
+    OR town LIKE :search
+    OR city LIKE :search
+    OR bands.band_name LIKE :search
+    OR countries.disp_name LIKE :search)";
+    $params["search"] = $search_term;
+    $render_params["search"] = $_GET['search'];
+}
 
 try {
-    $params=[];
-    if (isset($_GET['filter'])) {
-        $filter = "WHERE ".$_GET['filter']."_id = :filter_id";
-        $params = ["filter_id"=>$_GET['id']];
-    }
     $query = "SELECT
         show_id,
         ROW_NUMBER() OVER (
             ORDER BY show_date ASC
         ) row_num
     FROM shows
-    $filter
+    JOIN bands ON shows.band_id = bands.band_id
+    JOIN countries ON shows.country_id = countries.country_id
+    WHERE 1
+    $sql_filter
+    $search
      ORDER BY show_date ASC;";
-     $stmt = $db->prepare($query);
-     $stmt->execute($params);
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = $db->prepare($query);
+    $stmt->execute($params);
+    $render_params['shows'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 catch (PDO_EXCEPTION $e) {
     echo "Database error: ".$e->getMessage();
 }
 
-echo $m->render('gigography', $result);
+echo $m->render('gigography', $render_params);
