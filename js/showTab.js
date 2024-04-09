5a526f834e912729f3344044d58c68ebcf42388c
Nigel Powell <nigel@thesadsongco.com>
2024-04-09 07:35:38 +0100

    Slight tweak to tab transitions

--- js/showTab.js
+++ js/showTab.js
@@ -1,8 +1,8 @@
 const showTab = (event) => {
-  // const tabs = document.querySelectorAll('.tab');
-  // for (const tab of [...tabs]) {
-  //   tab.addEventListener('transitionend', showHideTab);
-  // }
+  const tabs = document.querySelectorAll('.tab');
+  for (const tab of [...tabs]) {
+    tab.addEventListener('transitionend', showHideTab);
+  }
   const targetTabSelector = event.target;
   const targetTab = document.getElementById(targetTabSelector.dataset.tab);
   const currentTabSelector = document.querySelector('.activeTab');
@@ -11,23 +11,6 @@ const showTab = (event) => {
   activateTabSelector(targetTabSelector);
   hideContentTab(currentTab);
   showContentTab(targetTab);
-  // const currentTab = document.querySelector('.visibleTab');
-  // if (currentTab) {
-  //   const currentTabSelectorId = currentTab.id + 'Tab';
-  //   const currentSelector = document.getElementById(currentTabSelectorId);
-  //   currentSelector.classList.remove('activeTab');
-  //   currentSelector.classList.add('inactiveTab');
-  //   currentTab.classList.remove('visibleTab');
-  //   currentTab.classList.add('hiddenTab');
-  // }
-  // const tabId = event.target.dataset.tab;
-  // const tabSelectorId = tabId + 'Tab';
-  // const targetEl = document.getElementById(tabId);
-  // targetEl.classList.remove('readyTab');
-  // targetEl.classList.add('visibleTab');
-  // const targetSelector = document.getElementById(tabSelectorId);
-  // targetSelector.classList.remove('inactiveTab');
-  // targetSelector.classList.add('activeTab');
 };
 
 const deactivateActiveTabSelector = (tab) => {
@@ -41,19 +24,17 @@ const activateTabSelector = (tab) => {
 };
 
 const showContentTab = (tab) => {
+  tab.classList.remove('noDisplay');
   tab.classList.remove('hiddenTab');
   tab.classList.add('visibleTab');
 };
 
 const hideContentTab = (tab) => {
-  console.log('hide', tab);
   tab.classList.remove('visibleTab');
   tab.classList.add('hiddenTab');
 };
 
 const showHideTab = (e) => {
   if (e.target.classList.contains('visibleTab')) return;
-  e.target.classList.add('readyTab');
-  e.target.classList.remove('hiddenTab');
-  console.log('hiding tab ' + e.target.id);
+  e.target.classList.add('noDisplay');
 };
