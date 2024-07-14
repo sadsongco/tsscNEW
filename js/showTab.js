8567f0af4878e0028f6682abad58296ccfde4f0e
Nigel Powell <nigel@thesadsongco.com>
2024-07-14 06:14:05 +0100

    blog linking

--- js/showTab.js
+++ js/showTab.js
@@ -39,3 +39,22 @@ const showHideTab = (e) => {
   if (e.target.classList.contains('visibleTab')) return;
   e.target.classList.add('noDisplay');
 };
+
+window.onload = () => {
+  // if it's a blog link, open the blog tab
+  const urlParams = new URLSearchParams(window.location.search);
+  const searchTab = urlParams.get('blog_id');
+  if (!searchTab) return;
+  const activeTab = document.querySelector('.activeTab');
+  const visibleTab = document.querySelector('.visibleTab');
+  activeTab.classList.remove('activeTab');
+  activeTab.classList.add('inactiveTab');
+  visibleTab.classList.remove('visibleTab');
+  visibleTab.classList.add('hiddenTab');
+  const blogTab = document.getElementById('blogTab');
+  const blog = document.getElementById('blog');
+  blogTab.classList.remove('inactiveTab');
+  blogTab.classList.add('activeTab');
+  blog.classList.remove('hiddenTab');
+  blog.classList.add('visibleTab');
+};
